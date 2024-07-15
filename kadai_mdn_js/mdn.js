af2fde9ebaf7cc254bd6f47ddbd69b8167f2392b
tatene9912 <t.atene0l4o1v2e@gmail.com>
2024-07-15 19:40:25 +0900

    Update mdn.js

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,6 +1,6 @@
 let today = new Date();
 let year = today.getFullYear();
-let month = today.getMonth();
-let day = today.getDay();
+let month = today.getMonth() +1;
+let day = today.getDate();
 
 console.log(year + '年' + month + '月' + day + '日')
